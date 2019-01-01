ba56908e30bc79a17413a915b10782053e463114
nipuns11 <nipunsh19@gmail.com>
2018-12-31 19:18:02 -0500

    Data driven framework
    Data driven framework intial login test

---
+++ src/test/java/com/w2a/testcases/LoginTest.java
@@ -0,0 +1,18 @@
+package com.w2a.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class LoginTest extends TestBase{
+
+	
+	@Test
+	public void loginAsbankManager() throws InterruptedException
+	{
+		driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
+		Thread.sleep(3000);
+	}
+	
+}
