2b3df2af5e573cbe152526e91deaffa9df78cacb
nipuns11 <nipunsh19@gmail.com>
2019-01-31 21:59:41 -0500

    reportng-imagescreenshot

--- src/test/java/com/w2a/testcases/BankManagerLoginTest.java
+++ src/test/java/com/w2a/testcases/BankManagerLoginTest.java
@@ -2,6 +2,7 @@ package com.w2a.testcases;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.w2a.base.TestBase;
@@ -14,8 +15,11 @@ public class BankManagerLoginTest extends TestBase {
 		driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
 		Thread.sleep(3000);
 		Assert.assertTrue(isElementpresent(By.cssSelector(OR.getProperty("addCustbtn"))), "login not succesfull");
-
-		log.debug("log sucessfull");
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		log.debug("log sucessfully executed");
+		Reporter.log("log sucessfully executed");
+		Reporter.log("<a target=\"blank\" href=\"C:\\Users\\NipunSharma\\Pictures\\albums\\april2016\\img006_26628216905_o.jpg\">Screenshot</a>");
+		
 	}
 
 }
